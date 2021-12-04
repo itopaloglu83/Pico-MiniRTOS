86add55d05ba105263c7460611319a9783973a18
Ihsan Topaloglu <itopaloglu83@gmail.com>
2021-12-04 16:30:28 -0500

    Minimized the example code.

--- minirtos.c
+++ minirtos.c
@@ -8,66 +8,35 @@
 #include "task.h"
 
 #define LED_PIN PICO_DEFAULT_LED_PIN
-#define RED_LED 0
 
-#define GPIO_ON 1
-#define GPIO_OFF 0
-
-void GreenLEDTask(void *param)
+void TaskBlink(void *pvParameters)
 {
     while (true)
     {
-        gpio_put(LED_PIN, GPIO_ON);
-        vTaskDelay(250);
-        gpio_put(LED_PIN, GPIO_OFF);
-        vTaskDelay(750);
-    }
-}
-
-void RedLEDTask(void *param)
-{
-    while (true)
-    {
-        gpio_put(RED_LED, GPIO_ON);
-        vTaskDelay(100);
-        gpio_put(RED_LED, GPIO_OFF);
-        vTaskDelay(100);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(750);
     }
 }
 
 int main()
 {
-    stdio_init_all();
-
+    // Setup GPIO.
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    gpio_init(RED_LED);
-    gpio_set_dir(RED_LED, GPIO_OUT);
+    // Create the initial task.
+    xTaskCreate(TaskBlink, "Blink", 128, NULL, 1, NULL);
 
-    TaskHandle_t gLEDtask = NULL;
-    TaskHandle_t rLEDtask = NULL;
-
-    uint32_t status = xTaskCreate(
-        GreenLEDTask,
-        "Green LED",
-        1024,
-        NULL,
-        tskIDLE_PRIORITY,
-        &gLEDtask);
-
-    status = xTaskCreate(
-        RedLEDTask,
-        "Red LED",
-        1024,
-        NULL,
-        1,
-        &rLEDtask);
-
+    // Start the scheduler.
+    // This never returns.
     vTaskStartScheduler();
 
-    for (;;)
+    while (true)
     {
-        //should never get here
+        // Program flow should not reach here.
     }
+    
+    return 0;
 }
